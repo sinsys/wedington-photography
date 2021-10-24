f190469ec646847406a11103873e1728efd2b8e8
Nicholas Hazel <nicholas.hazel@gmail.com>
2021-10-23 22:44:50 -0700

    feat: first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Viewport from './components/Viewport.svelte';
+</script>
+
+<main>
+	<Viewport />
+</main>
+
+<style type="scss">
+	@import './styles/base';
+	main {
+		@include page;
+		background: $baseBlue;
+		color: $light;
+	}
+</style>
---
+++ src/components/Viewport.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let name: string = 'Wedington Photography'
+</script>
+
+<div id="viewport">
+  {name}
+</div>
+
+<style lang="scss">
+  @import '../styles/base';
+  #viewport {
+    @include viewport;
+    @include flexCenterAll;
+    flex-direction: column;
+  }
+</style>
